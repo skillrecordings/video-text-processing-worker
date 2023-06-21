ec3c8dec960133f07c98489786899d703f37a795
Joel Hooks <joelhooks@gmail.com>
2023-06-21 10:01:58 -0700

    tune the code a bit

--- src/transcript.ts
+++ src/transcript.ts
@@ -1,23 +1,33 @@
-import { Inngest } from 'inngest'
+const deepgramUrl = `https://api.deepgram.com/v1/listen`
+const callbackUrl = `https://deepgram-wrangler.skillstack.workers.dev/transcriptComplete`
+
+//  deepgram config
+const utteranceSpiltThreshold = 0.5
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url)
 
-		let videoUrl = url.searchParams.get('videoUrl')
+		const videoUrl = url.searchParams.get('videoUrl')
 		const videoResourceId = url.searchParams.get('videoResourceId')
 		const siteName = url.searchParams.get('siteName') || 'epic-web'
-
-		console.log('videoUrl', videoUrl)
-		console.log('videoResourceId', videoResourceId)
 
 		if (!videoUrl) {
 			return new Response('Bad request: Missing `videoUrl` query param', { status: 400 })
 		}
 
-		const utteranceSpiltThreshold = 0.5
-		const deepgramUrl = `https://api.deepgram.com/v1/listen`
-		const callbackUrl = `https://deepgram-wrangler.skillstack.workers.dev/transcriptComplete`
+		if (!videoResourceId) {
+			return new Response('Bad request: Missing `videoResourceId` query param', { status: 400 })
+		}
+
+		if (!siteName) {
+			return new Response('Bad request: Missing `siteName` query param', { status: 400 })
+		}
+
+		const callbackParams = new URLSearchParams({
+			videoResourceId,
+			siteName,
+		})
 
 		const deepgramParams = new URLSearchParams({
 			model: 'whisper',
@@ -25,7 +35,7 @@ export default {
 			paragraphs: 'true',
 			utterances: 'true',
 			utt_split: String(utteranceSpiltThreshold),
-			callback: `${callbackUrl}?videoResourceId=${videoResourceId}&site=${siteName}`,
+			callback: `${callbackUrl}?${callbackParams.toString()}`,
 		})
 
 		const deepgramResponse = await fetch(`${deepgramUrl}?${deepgramParams.toString()}`, {
