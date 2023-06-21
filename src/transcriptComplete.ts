631f1ec437e7cf9f955bded18b7976108078e224
Taylor Bell <taylorbell@gmail.com>
2023-06-21 12:24:01 -0600

    update transcriptComplete for better srt

--- src/transcriptComplete.ts
+++ src/transcriptComplete.ts
@@ -1,17 +1,77 @@
 import { getInngest } from './get-inngest'
 
-function srtFromTranscriptResult(results: { utterances: { transcript: string; start: number; end: number }[] }) {
-	let srt = ``
+function convertTime(inputSeconds: number) {
+	const date = new Date(inputSeconds * 1000)
+	const hours = String(date.getUTCHours()).padStart(2, '0')
+	const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+	const seconds = String(date.getUTCSeconds()).padStart(2, '0')
+	const milliseconds = String(date.getUTCMilliseconds()).padStart(3, '0')
 
-	for (let i = 0; i < results.utterances.length; i++) {
-		const utterance = results.utterances[i]
-		const start = new Date(utterance.start * 1000).toISOString().substr(11, 12).replace('.', ',')
-		const end = new Date(utterance.end * 1000).toISOString().substr(11, 12).replace('.', ',')
-		srt += `${i + 1}\n${start} --> ${end}\n${utterance.transcript}\n\n`
-	}
-
-	return srt
+	return `${hours}:${minutes}:${seconds},${milliseconds}`
 }
+
+interface WordObject {
+	word: string
+	start: number
+	end: number
+	confidence: number
+	speaker: number
+	speaker_confidence: number
+	punctuated_word: string
+}
+
+function srtFromTranscriptResult(results: { channels: { alternatives: { words: WordObject[] }[] }[] }) {
+	let data = results.channels[0].alternatives[0].words
+
+	let arrayByTimes: WordObject[][] = []
+	let tempArray: WordObject[] = []
+
+	let timeLimitInSeconds = 5.5
+	let charLimit = 42
+	let currentTimeInSeconds = 0
+	let currentCharCount = 0
+
+	data.forEach((item, index) => {
+		let timeExceeded = currentTimeInSeconds + (item.end - item.start) >= timeLimitInSeconds
+		let charCountExceeded = currentCharCount + item.punctuated_word.length > charLimit
+
+		if (timeExceeded || charCountExceeded || index === data.length - 1) {
+			if (tempArray.length) {
+				arrayByTimes.push(tempArray)
+				tempArray = []
+				currentTimeInSeconds = 0
+				currentCharCount = 0
+			}
+		}
+
+		if (!timeExceeded || !charCountExceeded) {
+			tempArray.push(item)
+			currentTimeInSeconds += item.end - item.start
+			currentCharCount += item.word.length
+		}
+
+		// If the data is at the last item and the time or char count isn't exceeded, push the last item.
+		if (index === data.length - 1 && (!timeExceeded || !charCountExceeded)) {
+			arrayByTimes.push(tempArray)
+		}
+	})
+
+	let srtEntries = arrayByTimes.map((timeBlock, index) => {
+		let startTime = convertTime(timeBlock[0].start)
+		let endTime = convertTime(timeBlock[timeBlock.length - 1].end)
+		let text = timeBlock.map((x) => x.punctuated_word).join(' ')
+		let srtEntry = `${index + 1}
+${startTime} --> ${endTime}
+${text}
+		`
+
+		return srtEntry
+	})
+
+	let srtOutput = srtEntries.join('\n\n')
+	return srtOutput
+}
+
 
 function fullTranscriptFromResults(results: any) {
 	return results.channels[0].alternatives[0].paragraphs.transcript
