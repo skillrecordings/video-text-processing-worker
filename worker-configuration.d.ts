63a4b2be5cfc174dc2a6ad09af2fc9da6e1f6f16
joel <joelhooks@gmail.com>
2023-09-06 16:10:34 -0700

    remove some cruft

--- worker-configuration.d.ts
+++ worker-configuration.d.ts
@@ -4,18 +4,4 @@ interface Env {
 	EPIC_WEB_INNGEST_EVENT_KEY
 	EPIC_WEB_SANITY_TOKEN
 	EPIC_WEB_SANITY_PROJECT_ID
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
 }
